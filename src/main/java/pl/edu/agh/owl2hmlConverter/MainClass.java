52ebdb09e67c9082badc0bb25e8f3b784b69f023
gaxit <gaxit@interia.eu>
2015-01-10 17:03:43 +0100

    Dodano szkielet programu - wczytywanie, konwersję (przepisane wejście na wyjście) oraz zapisywanie do pliku

--- src/main/java/pl/edu/agh/owl2hmlConverter/MainClass.java
+++ src/main/java/pl/edu/agh/owl2hmlConverter/MainClass.java
@@ -1,13 +1,45 @@
 package pl.edu.agh.owl2hmlConverter;
 
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class MainClass 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {        
+        XmlFileReader xmlFileReader = new XmlFileReader();
+        Owl2HmlParser owl2HmlParser = new Owl2HmlParser();
+        XmlFileWriter xmlFileWriter = new XmlFileWriter();
         
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        Document owlDocument = null;
+        Document hmlDocument = null;
+        try {
+        	owlDocument = xmlFileReader.readFile();
+        	System.out.println("Owl file read");
+        	
+        	hmlDocument = owl2HmlParser.parseOwl2Hml(owlDocument);
+        	System.out.println("File converted");
+        	
+        	xmlFileWriter.writeXmlFile(hmlDocument);
+        	System.out.println("Hml file written");
+		} catch (ParserConfigurationException e) {
+			printErrorMessage(e);
+		} catch (SAXException e) {
+			printErrorMessage(e);
+		} catch (IOException e) {
+			printErrorMessage(e);
+		} catch (TransformerException e) {
+			printErrorMessage(e);
+		}
+    }
+    
+    private static void printErrorMessage(Exception e){
+    	System.out.println("ERROR: " + e.getMessage());
+    	e.printStackTrace();
     }
 }
