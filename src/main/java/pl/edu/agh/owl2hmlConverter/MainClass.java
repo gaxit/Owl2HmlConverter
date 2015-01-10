888e18f193f0ef495765ddf9dcce5419b7c9c3e7
gaxit <gaxit@interia.eu>
2015-01-10 16:09:01 +0100

    Pierwszy commit z dodanym libem do obslugi xml

---
+++ src/main/java/pl/edu/agh/owl2hmlConverter/MainClass.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.owl2hmlConverter;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class MainClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    }
+}
