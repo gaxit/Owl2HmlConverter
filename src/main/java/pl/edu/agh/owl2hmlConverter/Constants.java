fe4c2da56d2ae0ebb18acfe1b8ad5dc7165d9eae
gaxit <gaxit@interia.eu>
2015-01-18 01:41:45 +0100

    Dodano konwersję do hml tagów attributes i types

--- src/main/java/pl/edu/agh/owl2hmlConverter/Constants.java
+++ src/main/java/pl/edu/agh/owl2hmlConverter/Constants.java
@@ -20,4 +20,46 @@ public class Constants {
 	public interface AttributeNames{
 		public static final String IRI = "IRI";
 	}
+	
+	public interface HmlNodeNames{
+		public static final String HML = "hml";
+		public static final String TYPES = "types";
+		public static final String ATTRIBUTES = "attributes";
+		public static final String XTT = "xtt";
+		public static final String TYPE = "type";
+		public static final String DOMAIN = "domain";
+		public static final String VALUE = "value";
+		public static final String ATTRIBUTE = "attribute";
+	}
+	
+	public interface HmlAttributeNames{
+		public static final String ID = "id";
+		public static final String NAME = "name";
+		public static final String BASE = "base";
+		public static final String IS = "is";
+		public static final String NUM = "num";
+		public static final String TYPE = "type";
+		public static final String CLASS = "class";
+		public static final String COMM = "comm";
+	}
+	
+	public interface Additional{
+		public static final String TYPE_ID_BASE = "tpe_";
+		public static final String ATT_BASE = "att_";
+		public static final String SYMBOLIC = "symbolic";
+		public static final String SIMPLE = "simple";
+		public static final String IO = "io";
+	}
+	
+	public interface DtoSuffix{
+		public static final int INFERENCE_INDEX = 1;
+		public static final int DATA_TYPE_INDEX = 2;
+		public static final int SENSOR_INDEX = 3;
+	}
+	
+	public interface DtoNames{
+		public static final String INFERENCE_NAME = "Inference";
+		public static final String DATA_TYPE_NAME = "DataType";
+		public static final String SENSOR_NAME = "Sensor";
+	}
 }
--- src/main/java/pl/edu/agh/owl2hmlConverter/converters/ConvertedClassDTO2HmlConverter.java
+++ src/main/java/pl/edu/agh/owl2hmlConverter/converters/ConvertedClassDTO2HmlConverter.java
@@ -1,10 +1,15 @@
 package pl.edu.agh.owl2hmlConverter.converters;
 
+import java.util.List;
+
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
+import pl.edu.agh.owl2hmlConverter.Constants;
+import pl.edu.agh.owl2hmlConverter.classes.AbstractBaseDTO;
 import pl.edu.agh.owl2hmlConverter.classes.ConvertedClassDTO;
 import pl.edu.agh.owl2hmlConverter.utils.DocumentUtils;
 
@@ -12,11 +17,123 @@ public class ConvertedClassDTO2HmlConverter {
 
 	private static Document document;
 
-	public static Element convert(ConvertedClassDTO convertedClassDTO) throws ParserConfigurationException {
+	public static Element convert(ConvertedClassDTO convertedClassDTO)
+			throws ParserConfigurationException {
 		document = DocumentUtils.getDocument();
-		Element rootElement = document.createElement("company");
+		Element rootElement = document
+				.createElement(Constants.HmlNodeNames.HML);
+
+		// rootElement.appendChild(prepareTypesElement(convertedClassDTO));
+		// rootElement.appendChild(prepareAttributesElement());
+		rootElement.appendChild(prepareXttElement());
 
 		return rootElement;
+	}
+
+	private static Element prepareTypesElement(
+			ConvertedClassDTO convertedClassDTO) {
+		Element typesElement = document
+				.createElement(Constants.HmlNodeNames.TYPES);
+
+		typesElement.appendChild(prepareTypeElement(
+				Constants.DtoSuffix.INFERENCE_INDEX,
+				Constants.DtoNames.INFERENCE_NAME,
+				convertedClassDTO.getInferenceList()));
+
+		typesElement.appendChild(prepareTypeElement(
+				Constants.DtoSuffix.DATA_TYPE_INDEX,
+				Constants.DtoNames.DATA_TYPE_NAME,
+				convertedClassDTO.getDataTypeList()));
+
+		typesElement.appendChild(prepareTypeElement(
+				Constants.DtoSuffix.SENSOR_INDEX,
+				Constants.DtoNames.SENSOR_NAME,
+				convertedClassDTO.getSensorList()));
+
+		return typesElement;
+	}
+
+	private static Element prepareTypeElement(int id, String name,
+			List<? extends AbstractBaseDTO> dtoList) {
+		Element typeElement = document
+				.createElement(Constants.HmlNodeNames.TYPE);
+		int indexSuffix = 1;
+
+		typeElement.setAttribute(Constants.HmlAttributeNames.ID,
+				Constants.Additional.TYPE_ID_BASE + id);
+		typeElement.setAttribute(Constants.HmlAttributeNames.NAME, name);
+		typeElement.setAttribute(Constants.HmlAttributeNames.BASE,
+				Constants.Additional.SYMBOLIC);
+		typeElement.appendChild(prepareDomainElement(dtoList, indexSuffix));
+
+		return typeElement;
+	}
+
+	private static Node prepareDomainElement(
+			List<? extends AbstractBaseDTO> dtoList, int indexSuffix) {
+		Element domainElement = document
+				.createElement(Constants.HmlNodeNames.DOMAIN);
+		for (AbstractBaseDTO baseDTO : dtoList) {
+			domainElement
+					.appendChild(prepareValueElement(baseDTO, indexSuffix));
+			indexSuffix++;
+		}
+
+		return domainElement;
+	}
+
+	private static Node prepareValueElement(AbstractBaseDTO baseDTO,
+			int indexSuffix) {
+		Element valueElement = document
+				.createElement(Constants.HmlNodeNames.VALUE);
+
+		valueElement.setAttribute(Constants.HmlAttributeNames.IS,
+				baseDTO.getName());
+		valueElement.setAttribute(Constants.HmlAttributeNames.NUM,
+				String.valueOf(indexSuffix));
+
+		return valueElement;
+	}
+
+	private static Element prepareAttributesElement() {
+		Element attributesElement = document
+				.createElement(Constants.HmlNodeNames.ATTRIBUTES);
+
+		attributesElement.appendChild(prepareAttributeElement(
+				Constants.DtoSuffix.INFERENCE_INDEX,
+				Constants.DtoNames.INFERENCE_NAME));
+		attributesElement.appendChild(prepareAttributeElement(
+				Constants.DtoSuffix.DATA_TYPE_INDEX,
+				Constants.DtoNames.DATA_TYPE_NAME));
+		attributesElement.appendChild(prepareAttributeElement(
+				Constants.DtoSuffix.SENSOR_INDEX,
+				Constants.DtoNames.SENSOR_NAME));
+
+		return attributesElement;
+	}
+
+	private static Element prepareAttributeElement(int idSuffix, String name) {
+		Element attributeElement = document
+				.createElement(Constants.HmlNodeNames.ATTRIBUTE);
+
+		attributeElement.setAttribute(Constants.HmlAttributeNames.ID,
+				Constants.Additional.ATT_BASE + idSuffix);
+		attributeElement.setAttribute(Constants.HmlAttributeNames.TYPE,
+				Constants.Additional.TYPE_ID_BASE + idSuffix);
+		attributeElement.setAttribute(Constants.HmlAttributeNames.NAME, name);
+		attributeElement.setAttribute(Constants.HmlAttributeNames.CLASS,
+				Constants.Additional.SIMPLE);
+		attributeElement.setAttribute(Constants.HmlAttributeNames.COMM,
+				Constants.Additional.IO);
+
+		return attributeElement;
+	}
+
+	private static Element prepareXttElement() {
+		Element typesElement = document
+				.createElement(Constants.HmlNodeNames.XTT);
+
+		return typesElement;
 	}
 
 }
